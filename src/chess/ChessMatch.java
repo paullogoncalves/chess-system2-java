a1a0a1d948100bf7ca46359d0270c95039c4b041
Paulo Goncalves <paullogoncalves@gmail.com>
2020-12-19 16:35:52 -0300

    ChessException e ChessPosition2

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,8 +29,17 @@ public class ChessMatch {
     }
     
     private void initialSetup() {
-    	placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-    	placeNewPiece('e', 8, new King(board, Color.BLACK));
-    	placeNewPiece('e', 1, new King(board, Color.WHITE));
+    	 placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+         placeNewPiece('b', 1, new Rook(board, Color.WHITE));
+         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+         placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+         placeNewPiece('e', 1, new King(board, Color.WHITE));
+         placeNewPiece('f', 1, new Rook(board, Color.WHITE));
+         placeNewPiece('g', 1, new Rook(board, Color.WHITE));
+         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+         placeNewPiece('a', 2, new King(board, Color.WHITE));
+         placeNewPiece('b', 2, new King(board, Color.WHITE));
+         placeNewPiece('c', 2, new King(board, Color.WHITE));
+         placeNewPiece('d', 2, new King(board, Color.WHITE));
     }
 }
