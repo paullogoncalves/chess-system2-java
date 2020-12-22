ad5646734d82ed1dab4ef87787dbfd6ed0c0e2cc
Paulo Goncalves <paullogoncalves@gmail.com>
2020-12-21 22:01:01 -0300

    Movendo Pecas

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,21 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.positon = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
+		if (piece(position) == null) {
+			return null;
+		}
+		
+		Piece aux = piece(position);
+		aux.positon = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
