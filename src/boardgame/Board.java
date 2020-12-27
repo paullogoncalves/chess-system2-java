ecf4c50142a3f13215fcb52e54cc7f423c2b9b6d
Paulo Goncalves <paullogoncalves@gmail.com>
2020-12-27 10:12:39 -0300

    Handling captured pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
-			throw new BoardException("Error create board: the must be at least 1 row an 1 column");
+			throw new BoardException("Error Create board: the must be at least 1 row an 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
